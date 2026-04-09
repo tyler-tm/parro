8ed1493c7e267be53633b980fb2221e3402926cf
Tyler Maklebust <tyler@tylertm.com>
2026-04-08 22:06:15 -0700

    Conditionally compile for server or client

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
+#[cfg(feature = "client")]
 pub mod client;
 pub mod error;
+#[cfg(feature = "server")]
 mod handler;
 pub mod protocol;
+#[cfg(feature = "server")]
 pub mod server;
 mod static_utils;
+#[cfg(feature = "server")]
 mod storage;
 
 pub use serde::{Deserialize, Serialize};
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-use parro::error;
+#[cfg(feature = "server")]
 use parro::server::Server;
 
+#[cfg(feature = "server")]
 #[tokio::main]
-async fn main() -> error::Result<()> {
+async fn main() -> parro::error::Result<()> {
     let server = Server::new()
         .await
-        .map_err(|e| -> error::Error { format!("Error while starting server: {e}").into() })?;
+        .map_err(|e| -> parro::error::Error { format!("Error while starting server: {e}").into() })?;
     server.run().await
+}
+
+#[cfg(not(feature = "server"))]
+fn main() {
+    eprintln!("The server binary requires the `server` feature.");
+    std::process::exit(1);
 }
